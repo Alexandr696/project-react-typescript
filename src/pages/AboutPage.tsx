d3377c4408fe0eab999e36c4ee5ba1037caef04e
Alexander <52827777+Alexandr696@users.noreply.github.com>
2023-02-05 11:28:36 +0600

    table

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,5 +1,45 @@
 export function AboutPage () {
     return (
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dolorum modi odit provident quas. Accusamus architecto aut corporis cum debitis ducimus excepturi illum in, itaque maiores minus molestiae mollitia necessitatibus nisi odio, pariatur perspiciatis quia quibusdam quo repellat reprehenderit tempora veritatis. Consequuntur ex facere quas ratione reprehenderit. Debitis fugit, non!</div>
+        <div className="container mtd-3">
+        <div className="table-responsive">
+            <table className="table">
+                <tr>
+                    <th>Компания</th>
+                    <th>Телефон</th>
+                    <th>Телевизор</th>
+                    <th>Компьютер</th>
+                    <th>ОС</th>
+                </tr>
+                <tr>
+                    <td>Microsoft</td>
+                    <td></td>
+                    <td>нет</td>
+                    <td>да</td>
+                    <td>да</td>
+                </tr>
+                <tr>
+                    <td>Microsoft</td>
+                    <td>да</td>
+                    <td>нет</td>
+                    <td>да</td>
+                    <td>да</td>
+                </tr>
+                <tr>
+                    <td>Microsoft</td>
+                    <td>да</td>
+                    <td>нет</td>
+                    <td>да</td>
+                    <td>да</td>
+                </tr>
+                <tr>
+                    <td>Microsoft</td>
+                    <td>да</td>
+                    <td>нет</td>
+                    <td>да</td>
+                    <td>да</td>
+                </tr>
+            </table>
+        </div>
+    </div>
     )
 }
